9488d9cda1b42cd6088db8d0eb16e0d928bfefd3
estebanpablo89 <estebanpablo89@gmail.com>
2021-02-13 00:23:50 -0500

    port change

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const app = require('./app');
+const port = process.env.port || 3000;
 
-app.listen(3000, () => {
-  console.log('App listening on port 3000!');
+app.listen(port, () => {
+  console.log(`App listening on port ${port}!`);
 });
